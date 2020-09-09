b72dce2fffb426637fe2c6663cc1590502bd60f9
Neylanio <neylaniogomes@gmail.com>
2020-09-09 17:53:10 -0300

    feat: Private routes and to do logout

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -15,6 +15,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
+  loading: boolean;
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -23,6 +24,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void>{
@@ -33,6 +35,7 @@ const AuthProvider: React.FC = ({ children }) => {
       if(token[1] && user[1]){
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -64,7 +67,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
